664e27e1a021969a66fecbc97748c664eee6f5d5
Albi <alberto.candido91@gmail.com>
2021-01-12 20:53:23 +0100

    responsive video src added

---
+++ src-video-responsive.js
@@ -0,0 +1,26 @@
+//assegno le src dinamicamente
+if(window.innerWidth > 900){
+    //add higher quality srcs
+    document.getElementById('video-preview-madre').querySelector('source').src = "assets/video-preview/MuseoMadreNapoli.mp4";
+    document.getElementById('video-preview-italics').querySelector('source').src = "assets/video-preview/italics.mp4";
+    document.getElementById('video-preview-mm').querySelector('source').src = "assets/video-preview/mm.mp4";
+    document.getElementById('video-preview-combo').querySelector('source').src = "assets/video-preview/combo-2.mp4";
+    document.getElementById('video-preview-icon').querySelector('source').src = "assets/video-preview/icon.mp4";
+    document.getElementById('video-preview-grc').querySelector('source').src = "assets/video-preview/grc.mp4";
+    document.getElementById('video-preview-gs1').querySelector('source').src = "assets/video-preview/oi.mp4";
+    document.getElementById('video-preview-maxxi').querySelector('source').src = "assets/video-preview/maxxi.mp4";
+    document.getElementById('video-preview-land').querySelector('source').src = "assets/video-preview/land.mp4";
+    document.getElementById('video-preview-lft').querySelector('source').src = "assets/video-preview/lft.mp4";
+}else {
+  //add lower quality srcs
+    document.getElementById('video-preview-madre').querySelector('source').src = "assets/video-preview/mob/MuseoMadreNapoli.mp4";
+    document.getElementById('video-preview-italics').querySelector('source').src = "assets/video-preview/mob/italics.mp4";
+    document.getElementById('video-preview-mm').querySelector('source').src = "assets/video-preview/mob/mm.mp4";
+    document.getElementById('video-preview-combo').querySelector('source').src = "assets/video-preview/mob/combo-2.mp4";
+    document.getElementById('video-preview-icon').querySelector('source').src = "assets/video-preview/mob/icon.mp4";
+    document.getElementById('video-preview-grc').querySelector('source').src = "assets/video-preview/mob/grc.mp4";
+    document.getElementById('video-preview-gs1').querySelector('source').src = "assets/video-preview/mob/oi.mp4";
+    document.getElementById('video-preview-maxxi').querySelector('source').src = "assets/video-preview/mob/maxxi.mp4";
+    document.getElementById('video-preview-land').querySelector('source').src = "assets/video-preview/mob/land.mp4";
+    document.getElementById('video-preview-lft').querySelector('source').src = "assets/video-preview/mob/lft.mp4";
+}
